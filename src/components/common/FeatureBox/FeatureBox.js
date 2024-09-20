b990d87a621b6d89eb441f9dd51a183d49d99546
Patryk Bagan <patrykbn1997@gmail.com>
2024-09-20 17:02:02 +0200

    Converted FeatureBox to a link and added hover effect

--- src/components/common/FeatureBox/FeatureBox.js
+++ src/components/common/FeatureBox/FeatureBox.js
@@ -5,14 +5,20 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styles from './FeatureBox.module.scss';
 
-const FeatureBox = ({ active, icon, children }) => (
+const FeatureBox = ({ active, icon, children, link }) => (
   <div className={styles.root + (active ? ' ' + styles.active : '')}>
-    {icon && (
-      <div className={styles.iconWrapper}>
-        <FontAwesomeIcon className={styles.icon} icon={icon} />
-      </div>
-    )}
-    <div className={styles.content}>{children}</div>
+    <a
+      href={link}
+      className={styles.featurelink}
+      onClick={e => e.preventDefault()} //disabled click effect temporarily
+    >
+      {icon && (
+        <div className={styles.iconWrapper}>
+          <FontAwesomeIcon className={styles.icon} icon={icon} />
+        </div>
+      )}
+      <div className={styles.content}>{children}</div>
+    </a>
   </div>
 );
 
@@ -20,6 +26,7 @@ FeatureBox.propTypes = {
   children: PropTypes.node,
   icon: PropTypes.object,
   active: PropTypes.bool,
+  link: PropTypes.string,
 };
 
 export default FeatureBox;
