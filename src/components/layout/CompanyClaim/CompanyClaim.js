15a2a31afd618963eff2b77ac198c47a4e57477f
dave8git <dave8git@gmail.com>
2024-09-21 22:49:40 +0200

    Add fixed divs spaces && code for trimming more then 5 digits added

--- src/components/layout/CompanyClaim/CompanyClaim.js
+++ src/components/layout/CompanyClaim/CompanyClaim.js
@@ -8,37 +8,36 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMobileAlt, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
 
 const CompanyClaim = () => {
-  const [cartCount, setCartCount] = useState(99);
+  const [cartCount, setCartCount] = useState(99999999);
 
   const cartCounterClass = cartCount > 9 ? `${styles.cartCounter} ${styles.large}` : styles.cartCounter;
 
-
   return (
     <div className={styles.root}>
-    <div className='container'>
-      <div className='row align-items-center'>
-        <div className={`col text-left ${styles.phoneNumber}`}>
-          <p>
-            <FontAwesomeIcon className={styles.icon} icon={faMobileAlt} /> 2300 - 3560 -
-            222
-          </p>
-        </div>
-        <div className='col text-center'>
-          <a href='#'>
-            <img src='/images/logo.png' alt='Bazar' />
-          </a>
-        </div>
-        <div className={`col text-right ${styles.cart}`}>
-          <a href='#' className={styles.cartBox}>
-            <div className={styles.cartIcon}>
-              <FontAwesomeIcon className={styles.icon} icon={faShoppingBasket} />
-            </div>
-            <div className={cartCounterClass}>{cartCount}</div>
-          </a>
+      <div className='container'>
+        <div className='row align-items-center'>
+          <div className={`col text-left ${styles.phoneNumber}`}>
+            <p>
+              <FontAwesomeIcon className={styles.icon} icon={faMobileAlt} /> 2300 - 3560 -
+              222
+            </p>
+          </div>
+          <div className='col text-center'>
+            <a href='#'>
+              <img src='/images/logo.png' alt='Bazar' />
+            </a>
+          </div>
+          <div className={`col text-right ${styles.cart}`}>
+            <a href='#' className={styles.cartBox}>
+              <div className={styles.cartIcon}>
+                <FontAwesomeIcon className={styles.icon} icon={faShoppingBasket} />
+              </div>
+              <div className={cartCounterClass}>{cartCount.toString().slice(0, 5)}</div>
+            </a>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
 }
 
--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -5,6 +5,14 @@ export const getCount = ({ products }) => products.length;
 export const getNew = ({ products }) =>
   products.filter(item => item.newFurniture === true);
 
+/* action creators */
+const createActionName = actionName => `app/product/${actionName}`;
+const CHANGE_PRODUCT_FAVORITE = createActionName('CHANGE_PRODUCT_FAVORITE');
+const CHANGE_PRODUCT_COMPARE = createActionName('CHANGE_PRODUCT_COMPARE');
+
+export const changeFavorite = payload => ({ type: CHANGE_PRODUCT_FAVORITE, payload });
+export const changeCompare = payload => ({ type: CHANGE_PRODUCT_COMPARE, payload });
+
 /* reducer */
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
