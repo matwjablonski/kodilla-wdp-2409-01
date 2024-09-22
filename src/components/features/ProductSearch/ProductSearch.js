6af902c037ea8a91ae45e36f998ca7746af6e84b
dave8git <dave8git@gmail.com>
2024-09-22 16:01:05 +0200

    Add dropdown menu with categories now works and fetches categories from initial state

--- src/components/features/ProductSearch/ProductSearch.js
+++ src/components/features/ProductSearch/ProductSearch.js
@@ -5,14 +5,36 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faListUl, faSearch, faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './ProductSearch.module.scss';
+import { getCategories } from '../../../redux/productsRedux';
+import { useSelector } from 'react-redux';
 
-const ProductSearch = () => (
+const ProductSearch = () => {
+  
+  const categories = useSelector(getCategories);
+  
+  console.log('categories', categories);
+
+  return (
   <form action='' className={styles.root}>
     <div className={styles.category}>
+      {/* Move the list icon here before the dropdown */}
       <FontAwesomeIcon className={styles.icon} icon={faListUl} />
-      <select name='' id=''>
-        <option value=''>Select a category</option>
-      </select>
+      
+      <div>
+        <ul className={styles["dropdown"]}>
+          <li>
+            <a href="#">Select a category</a>
+            <ul className={styles["dropdown-content"]}>
+              {categories.map((category) => (
+                <li key={category.id}>
+                  <a href="#">{category.name}</a>
+                </li>
+              ))}
+            </ul>
+          </li>
+        </ul>
+      </div>
+
       <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
     </div>
     <div className={styles.searchField}>
@@ -22,7 +44,7 @@ const ProductSearch = () => (
       </button>
     </div>
   </form>
-);
+)};
 
 ProductSearch.propTypes = {
   children: PropTypes.node,
