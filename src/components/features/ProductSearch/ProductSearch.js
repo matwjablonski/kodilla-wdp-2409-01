0241650679ca1199db8a0fe8f6d7a69b19419028
dave8git <dave8git@gmail.com>
2024-09-23 13:08:44 +0200

    Fix styles and removed unnecessary console.log

--- src/components/features/ProductSearch/ProductSearch.js
+++ src/components/features/ProductSearch/ProductSearch.js
@@ -7,44 +7,41 @@ import { faListUl, faSearch, faCaretDown } from '@fortawesome/free-solid-svg-ico
 import styles from './ProductSearch.module.scss';
 import { getCategories } from '../../../redux/productsRedux';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom/cjs/react-router-dom';
 
 const ProductSearch = () => {
-  
   const categories = useSelector(getCategories);
-  
-  console.log('categories', categories);
 
   return (
-  <form action='' className={styles.root}>
-    <div className={styles.category}>
-      {/* Move the list icon here before the dropdown */}
-      <FontAwesomeIcon className={styles.icon} icon={faListUl} />
-      
-      <div>
-        <ul className={styles["dropdown"]}>
-          <li>
-            <a href="#">Select a category</a>
-            <ul className={styles["dropdown-content"]}>
-              {categories.map((category) => (
-                <li key={category.id}>
-                  <a href="#">{category.name}</a>
-                </li>
-              ))}
-            </ul>
-          </li>
-        </ul>
+    <form action='' className={styles.root}>
+      <div className={styles.category}>
+        {/* Move the list icon here before the dropdown */}
+        <FontAwesomeIcon className={styles.icon} icon={faListUl} />
+        <div>
+          <ul className={styles["dropdown"]}>
+            <li>
+              <a href="#">Select a category</a>
+              <ul className={styles["dropdown-content"]}>
+                {categories.map((category) => (
+                  <li key={category.id}>
+                    <Link>{category.name}</Link>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
       </div>
-
-      <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
-    </div>
-    <div className={styles.searchField}>
-      <input placeholder='Search products...' type='text' />
-      <button>
-        <FontAwesomeIcon className={styles.icon} icon={faSearch} />
-      </button>
-    </div>
-  </form>
-)};
+      <div className={styles.searchField}>
+        <input placeholder='Search products...' type='text' />
+        <button>
+          <FontAwesomeIcon className={styles.icon} icon={faSearch} />
+        </button>
+      </div>
+    </form>
+  )
+};
 
 ProductSearch.propTypes = {
   children: PropTypes.node,
