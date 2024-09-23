143d990690d08a41e51240ad87c92d1655e9b665
dave8git <dave8git@gmail.com>
2024-09-24 00:52:48 +0200

    Add added button functionality and modal

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -10,78 +10,117 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { faStar as farStar, faHeart } from '@fortawesome/free-regular-svg-icons';
 import Button from '../Button/Button';
-import { changeFavorite } from '../../../redux/productsRedux';
-import { useDispatch } from 'react-redux';
+import { changeFavorite, changeCompare, getCompare } from '../../../redux/productsRedux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
 
 
-const ProductBox = ({ id, name, price, promo, stars, compare, favorite, prevPrice }) => { 
+const ProductBox = ({ id, name, price, promo, stars, compare, favorite, prevPrice }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState('');
   const dispatch = useDispatch();
   const handleFavorite = e => {
     e.preventDefault();
     dispatch(changeFavorite(id));
   };
 
+  const Modal = ({ message, onClose }) => (
+    <div className="modal">
+      <div className="modal-content">
+        <p>{message}</p>
+        <button onClick={onClose}>Close</button>
+      </div>
+    </div>
+  );
+
+  const productsToCompare = useSelector((state) => getCompare(state));
+
+  const isCompared = productsToCompare.some(product => product.id === id);
+
+
+  const handleCompare = e => {
+  e.preventDefault();
+  if (!isCompared) {
+    if (productsToCompare.length < 4) {
+      dispatch(changeCompare(id));
+    } else {
+      setModalMessage('Two many items for comparison. Only 4 items are allowed.');
+      setIsModalOpen(true);
+    }
+  } else {
+    setModalMessage('Already in comparison.');
+    setIsModalOpen(true);
+  }
+};
+
+const closeModal = () => {
+  setIsModalOpen(false);
+};
+
   return (
 
-  <div className={styles.root}>
-    <div className={styles.photo}>
-      {promo && <div className={styles.sale}>{promo}</div>}
-      <div className={styles.buttons}>
-        <Button variant='small'>Quick View</Button>
-        <Button variant='small'>
-          <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO CART
-        </Button>
-      </div>
-    </div>
-    <div className={styles.content}>
-      <h5>{name}</h5>
-      <div className={styles.stars}>
-        {[1, 2, 3, 4, 5].map(i => (
-          <a key={i} href='#'>
-            {i <= stars ? (
-              <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
-            ) : (
-              <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
-            )}
-          </a>
-        ))}
-      </div>
-    </div>
-    <div className={styles.line}></div>
-    <div className={styles.actions}>
-      <div className={styles.outlines}>
-        <Button 
-          className={favorite && styles.active }
-          variant='outline'
-          >
-          <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
-        </Button>
-        <Button 
-           className={favorite && styles.active }
-          variant='outline'
-          >
-          <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
-        </Button>
-      </div>
-      <div className={styles.price}>
-        {prevPrice && ( 
-          <Button
-            variant='small'
-            noHover
-            className={styles.prevPrice}
-          >
-            $ {prevPrice}
+    <div className={styles.root}>
+      <div className={styles.photo}>
+        {promo && <div className={styles.sale}>{promo}</div>}
+        <div className={styles.buttons}>
+          <Button variant='small'>Quick View</Button>
+          <Button variant='small'>
+            <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO CART
           </Button>
-        )}
+        </div>
       </div>
-      <div className={styles.price}>
-        <Button noHover variant='small'>
-          $ {price}
-        </Button>
+      {isModalOpen && <Modal message={modalMessage} onClose={closeModal} />}
+      <div className={styles.content}>
+        <h5>{name}</h5>
+        <div className={styles.stars}>
+          {[1, 2, 3, 4, 5].map(i => (
+            <a key={i} href='#'>
+              {i <= stars ? (
+                <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
+              ) : (
+                <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
+              )}
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className={styles.line}></div>
+      <div className={styles.actions}>
+        <div className={styles.outlines}>
+          <Button
+            className={favorite && styles.active}
+            variant='outline'
+          >
+            <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
+          </Button>
+          <Button
+            className={compare && styles.active}
+            variant='outline'
+            onClick={handleCompare}
+          >
+            <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
+          </Button>
+        </div>
+        <div className={styles.price}>
+          {prevPrice && (
+            <Button
+              variant='small'
+              noHover
+              className={styles.prevPrice}
+            >
+              $ {prevPrice}
+            </Button>
+          )}
+        </div>
+        <div className={styles.price}>
+          <Button noHover variant='small'>
+            $ {price}
+          </Button>
+        </div>
       </div>
     </div>
-  </div>
-)};
+  )
+};
 
 ProductBox.propTypes = {
   children: PropTypes.node,
--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -5,20 +5,31 @@ export const getCount = ({ products }) => products.length;
 export const getNew = ({ products }) =>
   products.filter(item => item.newFurniture === true);
 
+export const getCompare = ({products}) => products.filter(product => product.compare === true);
+
 /* actions */ 
 const createActionName = actionName => `app/product/${actionName}`;
 const CHANGE_FAVORITE = createActionName('CHANGE_FAVORITE');
+const CHANGE_COMPARE = createActionName('CHANGE_COMPARE');
 
 /* action creator */ 
 export const changeFavorite = payload => ({
   type: CHANGE_FAVORITE, payload
 });
 
+export const changeCompare = payload => ({
+  type: CHANGE_COMPARE, payload
+})
+
+
+
 /* reducer */
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
     case CHANGE_FAVORITE: 
       return statePart.map(item => item.id === action.payload ? {...item, favorite: !item.favorite } : item )
+    case CHANGE_COMPARE: 
+      return statePart.map(item => item.id === action.payload ? {...item, compare: !item.compare } : item );
     default:
       return statePart;
   }
