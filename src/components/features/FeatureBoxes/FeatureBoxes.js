1055d2e04a91cec9720d3d72bbdca1fe552a65d9
dave8git <dave8git@gmail.com>
2024-09-24 15:42:20 +0200

    Fix RWD for FeatureBoxes

--- src/components/features/FeatureBoxes/FeatureBoxes.js
+++ src/components/features/FeatureBoxes/FeatureBoxes.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'; 
 import PropTypes from 'prop-types';
 
 import {
@@ -9,32 +9,33 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './FeatureBoxes.module.scss';
-import FeatureBox from '../../common/FeatureBox/FeatureBox';
+import FeatureBox from '../../common/FeatureBox/FeatureBox';    
 
 const FeatureBoxes = () => (
   <div className={styles.root}>
     <div className='container'>
-      <div className='row'>
-        <div className='col-12 col-sm-6 col-md-6 col-lg-3'>
-          <FeatureBox icon={faTruck} active link='placeHolder'>
+      <div className='row d-flex align-items-stretch'>
+        {/* Adjusted grid classes for responsive layout */}
+        <div className='col-6 col-sm-6 col-lg-3'>
+          <FeatureBox icon={faTruck} active link='placeHolder' className="h-100">
             <h5>Free shipping</h5>
             <p>All orders</p>
           </FeatureBox>
         </div>
-        <div className='col-12 col-sm-6 col-md-6 col-lg-3'>
-          <FeatureBox icon={faHeadphones} link='placeHolder'>
+        <div className='col-6 col-sm-6 col-lg-3'>
+          <FeatureBox icon={faHeadphones} link='placeHolder' className="h-100">
             <h5>24/7 customer</h5>
             <p>support</p>
           </FeatureBox>
         </div>
-        <div className='col-xs-6 col-sm-6 col-md-6 col-lg-3'>
-          <FeatureBox icon={faReplyAll} link='placeHolder'>
+        <div className='col-6 col-sm-6 col-lg-3'>
+          <FeatureBox icon={faReplyAll} link='placeHolder' className="h-100">
             <h5>Money back</h5>
             <p>guarantee</p>
           </FeatureBox>
         </div>
-        <div className='col-12 col-sm-6 col-md-6 col-lg-3'>
-          <FeatureBox icon={faBullhorn} link='placeHolder'>
+        <div className='col-6 col-sm-6 col-lg-3'>
+          <FeatureBox icon={faBullhorn} link='placeHolder' className="h-100">
             <h5>Member discount</h5>
             <p>First order</p>
           </FeatureBox>
@@ -49,4 +50,3 @@ FeatureBoxes.propTypes = {
 };
 
 export default FeatureBoxes;
-
