b249b62e8166626751ce56897f48c7ecebd299af
Patryk Bagan <patrykbn1997@gmail.com>
2024-09-26 21:41:35 +0200

    RWD for topbar and CompanyClaim

--- src/components/layout/CompanyClaim/CompanyClaim.js
+++ src/components/layout/CompanyClaim/CompanyClaim.js
@@ -9,19 +9,19 @@ import { faMobileAlt, faShoppingBasket } from '@fortawesome/free-solid-svg-icons
 const CompanyClaim = () => (
   <div className={styles.root}>
     <div className='container'>
-      <div className='row align-items-center'>
-        <div className={`col text-left ${styles.phoneNumber}`}>
+      <div className={styles.gridContainer}>
+        <div className={`${styles.phoneNumber} ${styles.numberGrid}`}>
           <p>
             <FontAwesomeIcon className={styles.icon} icon={faMobileAlt} /> 2300 - 3560 -
             222
           </p>
         </div>
-        <div className='col text-center'>
+        <div className={styles.logoGrid}>
           <a href='#'>
             <img src='/images/logo.png' alt='Bazar' />
           </a>
         </div>
-        <div className={`col text-right ${styles.cart}`}>
+        <div className={`${styles.cart} ${styles.cartGrid}`}>
           <a href='#' className={styles.cartBox}>
             <div className={styles.cartIcon}>
               <FontAwesomeIcon className={styles.icon} icon={faShoppingBasket} />
