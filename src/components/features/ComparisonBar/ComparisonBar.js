70da732695479a70bf9c8cdf0cd64b66f54d913c
dave8git <dave8git@gmail.com>
2024-09-26 14:36:42 +0200

    Fix changes in NewFurniture.js component #2

--- src/components/features/ComparisonBar/ComparisonBar.js
+++ src/components/features/ComparisonBar/ComparisonBar.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getCompare, changeCompare } from '../../../redux/productsRedux';
 import styles from './ComparisonBar.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const ComparisonBar = () => {
   const selectedProducts = useSelector(state => getCompare(state));
-  const dispatch = useDispatch(); 
+  const dispatch = useDispatch();
   if (selectedProducts.length === 0) return null;
 
   const handleFavoriteToggle = (productId) => {
-    dispatch(changeCompare(productId)); 
+    dispatch(changeCompare(productId));
   };
 
   return (
@@ -17,14 +19,26 @@ const ComparisonBar = () => {
       <div className={styles.productRow}>
         {selectedProducts.map(product => (
           <div key={product.id} className={styles.productItem}>
-            <img 
-              src={`/images/productBox/${product.backgroundPhoto}`} 
-              alt={product.name} 
-              className={styles.productImage} 
-              onClick={() => handleFavoriteToggle(product.id)} 
-            />
+            <div className={styles.imageContainer}>
+              <img
+                src={`/images/productBox/${product.backgroundPhoto}`}
+                alt={product.name}
+                className={styles.productImage}
+                onClick={() => handleFavoriteToggle(product.id)}
+              />
+              <button
+                className={styles.trashButton}
+                // onClick={() => handleFavoriteToggle(product.id)}
+              >
+                <FontAwesomeIcon icon={faTrash} />
+              </button>
+            </div>
           </div>
         ))}
+        <button className={styles.compareButton}>
+          {/* <FontAwesomeIcon icon={faArrowRight} className={styles.compareIcon} /> */}
+            <span>Compare</span>
+        </button>
       </div>
     </div>
   );
