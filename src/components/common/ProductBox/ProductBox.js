1c995f32dbce1b00edfa5904ad0a8aee4f84bec3
Patryk Bagan <patrykbn1997@gmail.com>
2024-09-28 11:07:25 +0200

    test commit for PM

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -19,81 +19,82 @@ const ProductBox = ({ id, name, price, promo, stars, backgroundPhoto, compare, f
     dispatch(changeFavorite(id));
   };
   return (
-  <div className={styles.root}>
-    <div className={styles.photo}>
-      <div>
-        <img
-          src={`/images/productBox/${backgroundPhoto}`}
-          alt='Product Photo'
-          className={styles.image}
-        />
+    <div className={styles.root}>
+      <div className={styles.photo}>
+        <div>
+          <img
+            src={`/images/productBox/${backgroundPhoto}`}
+            alt='Product Photo'
+            className={styles.image}
+          />
+        </div>
+        {promo && <div className={styles.sale}>{promo}</div>}
       </div>
-      {promo && <div className={styles.sale}>{promo}</div>}
-    </div>
-    <div className={styles.buttons}>
-      <Button variant='small'>Quick View</Button>
-      <Button variant='small'>
-        <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO CART
-      </Button>
-    </div>
-    <div className={styles.content}>
-      <h5>{name}</h5>
-      <div className={styles.stars}>
-        {[1, 2, 3, 4, 5].map(i => (
-          <a key={i} href='#'>
-            {i <= stars ? (
-              <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
-            ) : (
-              <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
-            )}
-          </a>
-        ))}
-      </div>
-    </div>
-    <div className={styles.line}></div>
-    <div className={styles.actions}>
-      <div className={styles.outlines}>
-        <Button 
-          variant='outline'
-          onClick={handleFavorite}
-           className={favorite && styles.active }
-        >
-
-          <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
-        </Button>
-        <Button className={compare && styles.active} variant='outline'>
-          <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
+      <div className={styles.buttons}>
+        <Button variant='small'>Quick View</Button>
+        <Button variant='small'>
+          <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO CART
         </Button>
       </div>
-      <div className={styles.price}>
-        {prevPrice && (
-          <Button
-            variant='small'
-            noHover
-            className={`${styles.prevPrice} ${styles.priceButton}`}
+      <div className={styles.content}>
+        <h5>{name}</h5>
+        <div className={styles.stars}>
+          {[1, 2, 3, 4, 5].map(i => (
+            <a key={i} href='#'>
+              {i <= stars ? (
+                <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
+              ) : (
+                <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
+              )}
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className={styles.line}></div>
+      <div className={styles.actions}>
+        <div className={styles.outlines}>
+          <Button 
+            variant='outline'
+            onClick={handleFavorite}
+            className={favorite && styles.active }
           >
-            $ {prevPrice}
+
+            <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
           </Button>
-        )}
-        <Button noHover variant='small' className={styles.priceButton}>
+          <Button className={compare && styles.active} variant='outline'>
+            <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
+          </Button>
+        </div>
+        <div className={styles.price}>
+          {prevPrice && (
+            <Button
+              variant='small'
+              noHover
+              className={`${styles.prevPrice} ${styles.priceButton}`}
+            >
+            $ {prevPrice}
+            </Button>
+          )}
+          <Button noHover variant='small' className={styles.priceButton}>
           $ {price}
-        </Button>
+          </Button>
+        </div>
       </div>
     </div>
-  </div>
-)};
+  );
+};
 
 ProductBox.propTypes = {
   children: PropTypes.node,
+  id: PropTypes.string,
   name: PropTypes.string,
   price: PropTypes.number,
   promo: PropTypes.string,
   stars: PropTypes.number,
-  compare: PropTypes.string,
+  compare: PropTypes.bool,
   favorite: PropTypes.bool,
   backgroundPhoto: PropTypes.string,
   prevPrice: PropTypes.number,
-  favorite: PropTypes.bool,
 };
 
 export default ProductBox;
